9de554cfd0b367b94688160961fef2d4d4f68dd5
aylee1 <86951396+ayleeee@users.noreply.github.com>
2022-05-01 21:35:18 +0900

    Time: 3 ms (95.58%), Space: 8 MB (49.30%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1,0);
+        if(n==0) return {0};
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            if(i%2==0) dp[i]=dp[i/2];
+            else dp[i]=1+dp[i/2];
+        }
+        return dp;
+    }
+};
+
