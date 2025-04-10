3c17dc78934313412debf67148e70d165110ebb5
ayleeee <86951396+ayleeee@users.noreply.github.com>
2025-04-10 16:34:32 +0900

    Time: 0 ms (100%), Space: 19.9 MB (8.76%) - LeetHub

---
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        stack = []
+        # 최소값, 최대값 구할 때 사용
+        # float('-inf') : 음의 무한대
+        # float('inf') : 양의 무한대
+        prev = float('-inf')
+        current = root
+
+        # current가 존재하거나 stack이 비어있지 않은 한 계속 지속
+        while current or stack : 
+            while current:
+                stack.append(current)
+                current = current.left
+            current = stack.pop()
+            if current.val <= prev:
+                return False
+            prev = current.val
+            current = current.right
+        return True
+            
+        
